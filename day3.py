46f88848700bdbd8deeabd48cc2f02eb0613dc07
Devesh Singh <zero2march@gmail.com>
2026-08-14 16:58:33 +0530

    Update Everything

---
+++ day3.py
@@ -0,0 +1,11 @@
+a=10
+b=20
+print(a+b) #30
+print(a-b) #-10
+print(a*b) #200
+print(a%b) #10
+print(a==b) # False
+print(a>b and a==b) #False
+print(a>b or a==b) #False
+print(a<b or a==b) #True
+print(not a) #False
