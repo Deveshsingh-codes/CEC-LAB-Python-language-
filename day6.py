8d3756b3d7db4d9ff5304d4ed59e66fba0d83f13
Devesh Singh <zero2march@gmail.com>
2026-08-21 16:55:36 +0530

    Update day6

--- day6.py
+++ day6.py
@@ -1,44 +1,44 @@
-#num = int(input("Enter the Number = "))
-#num = float(input("Enter the Number = "))
-#if num>0:
-#    print("Positive number.")
-#elif num<0:
-#    print("Negative number.")
-#else:
-#    print("Number is zero.")
-#    
+num = int(input("Enter the Number = "))
+num = float(input("Enter the Number = "))
+if num>0:
+    print("Positive number.")
+elif num<0:
+    print("Negative number.")
+else:
+    print("Number is zero.")
+    
 
 # <---- BY USING OPERATORS ---->
 
-#Students_Marks = int(input("Enter the Marks of Subject: "))
-#
-#if Students_Marks >= 90:
-#    print("Excellent.")
-#
-#elif Students_Marks >= 75:
-#    print("PASS.")
-#
-#elif Students_Marks >= 40:
-#    print("Needs to improve.")
-#
-#else:
-#    print("FAIL.")
-#
-##<--- BY USING AND AND OR --->
-#
-#Students_Marks = int(input("Enter the Marks of Subject: "))
-#
-#if Students_Marks >= 90 and Students_Marks <= 100:
-#    print("Excellent.")
-#
-#elif Students_Marks >= 75 and Students_Marks < 90:
-#    print("PASS.")
-#
-#elif Students_Marks >= 40 and Students_Marks < 75:
-#    print("Needs to improve.")
-#
-#else:
-#    print("FAIL.")
+Students_Marks = int(input("Enter the Marks of Subject: "))
+
+if Students_Marks >= 90:
+    print("Excellent.")
+
+elif Students_Marks >= 75:
+    print("PASS.")
+
+elif Students_Marks >= 40:
+    print("Needs to improve.")
+
+else:
+    print("FAIL.")
+
+#<--- BY USING AND AND OR --->
+
+Students_Marks = int(input("Enter the Marks of Subject: "))
+
+if Students_Marks >= 90 and Students_Marks <= 100:
+    print("Excellent.")
+
+elif Students_Marks >= 75 and Students_Marks < 90:
+    print("PASS.")
+
+elif Students_Marks >= 40 and Students_Marks < 75:
+    print("Needs to improve.")
+
+else:
+    print("FAIL.")
 
 Student_Name=str(input("Enter your Name :"))
 Student_marks=int(input("Enter the Marks :"))
@@ -56,3 +56,14 @@ elif Student_marks >= 40 and Student_marks < 75:
 else:
     print("FAIL.")
 
+Num1 = int(input("Enter Num1: "))
+Num2 = int(input("Enter Num2: "))
+
+if Num1 > Num2:
+    print(Num1, "is greater than", Num2)
+
+elif Num2 > Num1:
+    print(Num2, "is greater than", Num1)
+
+else:
+    print("Both numbers are equal.")
